5cd961a31a41329a9bfb46916fb9ea4f03bbbcad
Jeremy <ao31746@gmail.com>
2021-06-25 15:21:46 +0800

    convert to java

--- smart_app/lib/main.dart
+++ smart_app/lib/main.dart
@@ -1,5 +1,4 @@
-// import 'dart:html';
-
+// @Dart=2.9
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -14,9 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xff94A791),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: '智慧工地監測系統'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -56,48 +64,63 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('智慧工地管理系統'),
+      ),
       body: WebView(
-        initialUrl: "https://smartapptest.kingsu.com.tw/index/wapp?sendtoken=637009530995528690",
-        javascriptMode: JavascriptMode.unrestricted,
-      )
+        initialUrl: 'https://smartapptest.kingsu.com.tw/index/wapp?sendtoken=sendtoken',
+        javascriptMode: JavascriptMode.unrestricted
+      ),
     );
-    // bool _checkbox = false;
+
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
     //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     // return Scaffold(
-    //     body: Center(
-    //   child: Column(
-    //     children: <Widget>[
-    //       Image.asset('assets/login_logo.png'),
-    //       Text("智慧工地監測系統"),
-    //       Text("Monitoring System"),
-    //       TextField(
-    //           decoration: InputDecoration(
-    //               border: OutlineInputBorder(),
-    //               labelText: '帳號',
-    //               hintText: '帳號')),
-    //       TextField(
-    //           decoration: InputDecoration(
-    //               border: OutlineInputBorder(),
-    //               labelText: '密碼',
-    //               hintText: '密碼')),
-    //       Checkbox(
-    //         value: _checkbox,
-    //         onChanged: (value) {
-    //           setState(() {
-    //             _checkbox = !_checkbox;
-    //           });
-    //         },
-    //       ),
-    //       TextButton(
-    //         style: TextButton.styleFrom(
-    //           primary: Colors.blue,
-    //         ),
-    //         onPressed: () {},
-    //         child: Text('登入'),
-    //       ),
-    //       Text("金粟科技工程顧問有限公司 開發"),
-    //     ],
+    //   appBar: AppBar(
+    //     // Here we take the value from the MyHomePage object that was created by
+    //     // the App.build method, and use it to set our appbar title.
+    //     title: Text(widget.title),
     //   ),
-    // ));
+    //   body: Center(
+    //     // Center is a layout widget. It takes a single child and positions it
+    //     // in the middle of the parent.
+    //     child: Column(
+    //       // Column is also a layout widget. It takes a list of children and
+    //       // arranges them vertically. By default, it sizes itself to fit its
+    //       // children horizontally, and tries to be as tall as its parent.
+    //       //
+    //       // Invoke "debug painting" (press "p" in the console, choose the
+    //       // "Toggle Debug Paint" action from the Flutter Inspector in Android
+    //       // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+    //       // to see the wireframe for each widget.
+    //       //
+    //       // Column has various properties to control how it sizes itself and
+    //       // how it positions its children. Here we use mainAxisAlignment to
+    //       // center the children vertically; the main axis here is the vertical
+    //       // axis because Columns are vertical (the cross axis would be
+    //       // horizontal).
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         Text(
+    //           'You have pushed the button this many times:',
+    //         ),
+    //         Text(
+    //           '$_counter',
+    //           style: Theme.of(context).textTheme.headline4,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
   }
 }
