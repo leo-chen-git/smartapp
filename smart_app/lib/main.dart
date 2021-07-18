e0ee667543e98ba32d600b914b8b603273c0c3f0
Jeremy <ao31746@gmail.com>
2021-07-18 09:38:51 +0800

    geometry and upload js enable

--- smart_app/lib/main.dart
+++ smart_app/lib/main.dart
@@ -1,15 +1,42 @@
+// @Dart=2.9
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:location_permissions/location_permissions.dart';
+
 
 String fcmToken = "";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  /// Create an Android Notification Channel.
+  /// 這裡有多使用套件: flutter_local_notifications: ^3.0.3
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  // AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  // requesting permission
+  PermissionStatus requestPermissions = await LocationPermissions().requestPermissions();
+  PermissionStatus checkPermissionStatus = await LocationPermissions().checkPermissionStatus();
+  print("PermissionStatus:"+ requestPermissions.toString());
+  print("checkPermissionStatus:"+ checkPermissionStatus.toString());
 
   late FirebaseMessaging messaging;
 
@@ -34,103 +61,100 @@ void main() async {
       fcmToken = value;
     }
     print("fcm token:"+value!);
+    runApp(MyApp());
   });
-  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print("Handling a background message ${message.messageId}");
+}
 
+
+
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Notification.instance.setContext(context);
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          body: MyHomePage(),
-        ),
-      ),
+      title: 'SmartApp',
+      home: MyHomePage(title: 'SmartApp Home Page'),
     );
   }
+
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  static final location = Location();
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // late FirebaseMessaging messaging;
+  // String fcmToken = "";
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<PermissionStatus>(
-        future: location.hasPermission(),
-        builder: (context, snapshot) {
-          print('permission: ${snapshot.data}');
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
+    print(""
+        ".build: " + 'https://smartapp.kingsu.com.tw/index/wapp?sendtoken=$fcmToken');
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(10.0), // here the desired height
+          child: AppBar(
+            backgroundColor: Colors.black,
+          )
+      ),
 
-          final status = snapshot.data!;
-          final permit = status == PermissionStatus.granted ||
-              status == PermissionStatus.grantedLimited;
+      body: WebView(
+        initialUrl: 'https://smartapp.kingsu.com.tw/index/wapp?sendtoken=$fcmToken',
+        javascriptMode: JavascriptMode.unrestricted,
+        gestureNavigationEnabled: true,
 
-          return permit ? _webView() : _request();
-        });
-  }
 
-  Widget _webView() {
-    print('start web view');
-
-    return WebView(
-      initialUrl: 'https://smartapp.kingsu.com.tw/index/wapp?sendtoken=$fcmToken',
-      javascriptMode: JavascriptMode.unrestricted,
+      )
+      // body: WebviewScaffold(
+      //   url: 'https://smartapp.kingsu.com.tw/index/wapp?sendtoken=$fcmToken',
+      //   withLocalStorage: true,
+      //   withZoom: true,
+      //   geolocationEnabled: true,
+      //   withJavascript: true,
+      // ),
     );
-  }
-
-  Widget _request() {
-    print('start request permission');
-
-    return FutureBuilder(
-        future: Future.wait([
-          location.requestPermission(),
-          FirebaseMessaging.instance.requestPermission(),
-        ]),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          return _webView();
-        });
-  }
-}
-
-class Notification {
-  static final Notification instance = Notification._();
-
-  BuildContext? context;
-
-  void setContext(BuildContext context) {
-    if (this.context == null) {
-      this.context = context;
-    }
-  }
-
-  Notification._() {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      if (message.notification != null) {
-        final messages = <String>[
-          message.notification?.title ?? '',
-          message.notification?.body ?? '',
-        ].where((e) => e != '').map<Widget>((e) => Text(e));
-
-        if (messages.isEmpty) return;
-
-        final snackBar = SnackBar(content: Column(children: messages.toList()));
-
-        ScaffoldMessenger.of(context!).showSnackBar(snackBar);
-      }
-    });
   }
 }
---
+++ smart_app/plugins/webview_flutter/README.md
@@ -0,0 +1,1 @@
+# 支持文件上传的flutter_webview
